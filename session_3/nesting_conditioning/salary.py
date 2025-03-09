5434923531339dbacba8fa05b473491123c1a00b
mohfer <nastyraven123@gmail.com>
2025-03-09 08:45:16 +0700

    refactor: simplify salary calculation logic and improve variable naming

--- session_3/nesting_conditioning/salary.py
+++ session_3/nesting_conditioning/salary.py
@@ -1,48 +1,44 @@
-permanent_employee_salary = 1000000
-honorary_employee_salary = 750000
+permanent_salary = 1000000
+honorary_salary = 750000
 
-bonus_permanent_employee_group_a = 200000
-bonus_permanent_employee_group_b = 400000
-bonus_permanent_employee_group_c = 550000
+permanent_bonus_a = 200000
+permanent_bonus_b = 400000
+permanent_bonus_c = 550000
 
-bonus_honorary_employee_group_a = 150000
-bonus_honorary_employee_group_b = 275000
-bonus_honorary_employee_group_c = 480000
+honorary_bonus_a = 150000
+honorary_bonus_b = 275000
+honorary_bonus_c = 480000
 
 name = input("Masukkan nama: ")
 nik = input("Masukkan NIK: ")
-
-while True:
-    status = input("Masukkan status pegawai (Tetap/Honorer): ").lower()
-    if status in ["tetap", "honorer"]:
-        break
-    print("Status tidak valid. Harap masukkan 'Tetap' atau 'Honorer'.")
+status = input("Masukkan status pegawai (Tetap/Honorer): ").lower()
+group = input("Masukkan golongan (A/B/C): ").lower()
 
 if status == "tetap":
-    salary = permanent_employee_salary
-else:
-    salary = honorary_employee_salary
-
-while True:
-    group = input("Masukkan golongan (A/B/C): ").lower()
-    if group in ["a", "b", "c"]:
-        break
-    print("Golongan tidak valid. Harap masukkan 'A', 'B', atau 'C'.")
-
-if status == "tetap":
+    salary = permanent_salary
     if group == "a":
-        bonus = bonus_permanent_employee_group_a
+        bonus = permanent_bonus_a
     elif group == "b":
-        bonus = bonus_permanent_employee_group_b
+        bonus = permanent_bonus_b
     elif group == "c":
-        bonus = bonus_permanent_employee_group_c
-else:
+        bonus = permanent_bonus_c
+    else:
+        print("Golongan tidak valid!")
+        exit()
+elif status == "honorer":
+    salary = honorary_salary
     if group == "a":
-        bonus = bonus_honorary_employee_group_a
+        bonus = honorary_bonus_a
     elif group == "b":
-        bonus = bonus_honorary_employee_group_b
+        bonus = honorary_bonus_b
     elif group == "c":
-        bonus = bonus_honorary_employee_group_c
+        bonus = honorary_bonus_c
+    else:
+        print("Golongan tidak valid!")
+        exit()
+else:
+    print("Status pegawai tidak valid!")
+    exit()
 
 print(f"\nNama: {name}")
 print(f"NIK: {nik}")
